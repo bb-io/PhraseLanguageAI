a6c0d76259a27de52380455921fa51207dee328f
Fesenko-A <111450737+Fesenko-A@users.noreply.github.com>
2026-05-12 11:23:45 +0300

    update connections order (backward compatibility)

--- Apps.PhraseLanguageAI/Connections/ConnectionDefinition.cs
+++ Apps.PhraseLanguageAI/Connections/ConnectionDefinition.cs
@@ -11,8 +11,29 @@ public class ConnectionDefinition : IConnectionDefinition
     {
         new()
         {
+            Name = ConnectionTypes.ApiKey,
+            AuthenticationType = ConnectionAuthenticationType.Undefined,
+            ConnectionProperties = new List<ConnectionProperty>
+            {
+                new(CredsNames.UserName) { DisplayName = "User name"},
+                new(CredsNames.Password) { DisplayName = "Password" , Sensitive = true },
+                new(CredsNames.Url) { DisplayName = "Data center URL",
+                Description="Select the base URL according to your Phrase data center",
+                DataItems = 
+                    [
+                        new("https://eu.phrase.com/smt/api/", "EU data center (Production)"),
+                        new("https://us.phrase.com/smt/api/", "US data center (Production)"),
+                        new("https://eu.phrase-staging.com/smt/api/", "EU data center (Staging)"),
+                        new("https://us.phrase-staging.com/smt/api/", "US data center (Staging)"),
+                    ]
+                },
+                new(CredsNames.OrganizationId) { DisplayName = "Organization ID", Description = "Enter the organization ID" }
+            }
+        },
+        new()
+        {
             Name = ConnectionTypes.ApiToken,
-            DisplayName = "API Token",
+            DisplayName = "Platform API Token (recommended)",
             AuthenticationType = ConnectionAuthenticationType.Undefined,
             ConnectionProperties = new List<ConnectionProperty>
             {
@@ -29,27 +50,6 @@ public class ConnectionDefinition : IConnectionDefinition
                     ]
                 },
                 new(CredsNames.ApiToken) { DisplayName = "API Token", Sensitive = true }
-            }
-        },
-        new()
-        {
-            Name = ConnectionTypes.ApiKey,
-            AuthenticationType = ConnectionAuthenticationType.Undefined,
-            ConnectionProperties = new List<ConnectionProperty>
-            {
-                new(CredsNames.UserName) { DisplayName = "User name"},
-                new(CredsNames.Password) { DisplayName = "Password" , Sensitive=true},
-                new(CredsNames.Url) { DisplayName = "Data center URL",
-                Description="Select the base URL according to your Phrase data center",
-                DataItems = 
-                    [
-                        new("https://eu.phrase.com/smt/api/", "EU data center (Production)"),
-                        new("https://us.phrase.com/smt/api/", "US data center (Production)"),
-                        new("https://eu.phrase-staging.com/smt/api/", "EU data center (Staging)"),
-                        new("https://us.phrase-staging.com/smt/api/", "US data center (Staging)"),
-                    ]
-                },
-                new(CredsNames.OrganizationId) { DisplayName = "Organization ID", Description = "Enter the organization ID" }
             }
         }
     };
